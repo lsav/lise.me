94bf282e3881556430a737f523dd479cf296fe19
Lise Savard <lise@savard.cc>
2016-09-03 08:15:55 +0000

    Changed font to Source Sans Pro

--- rakefile.rb
+++ rakefile.rb
@@ -1,9 +1,36 @@
+# rakefile to automate creation & publication of Jekyll posts
+# accepted commands:
+# - (default) rake draft => prompt for a name, then create a draft with that name in /_drafts
+# - rake publish => list all the drafts that are ready for publishing and prompt for which one to publish
+# - rake publish all => publish all the drafts in /_drafts
+
 require 'time'
 
 # Prompt for title & format it correctly
 def get_input(str)
   print str
   STDIN.gets.strip
+end
+
+# Publish a draft
+def publish_draft(source)
+  # create new file with with timestamp in _posts directory
+  cur_time = Time.now.getlocal("-07:00")
+  target = "_posts/#{cur_time.strftime('%Y-%m-%d')}-#{source.gsub('_drafts/', '')}"
+  File.open(target, 'w') do |file| file.write <<-EOS
+---
+date: #{Time.now.getlocal("-07:00")}
+EOS
+  end
+  
+  # copy draft over, but remove the first line
+  File.readlines(source).drop(1).each do |line| 
+    File.open(target, "a+") { |file| file.write(line) }
+  end
+  
+  # delete the draft & return path to new post
+  File.delete(source)
+  target
 end
 
 # Create new draft
@@ -30,32 +57,31 @@ end
 desc "Publish existing draft"
 task :publish do
   drafts = Dir.glob("_drafts/*")
-  if drafts.length == 0
+  num = drafts.length
+  
+  if num == 0
     answer = get_input('No drafts to publish. Create a new draft? ')
     if ['y', 'yes', 'Yes'].include? answer
       Rake::Task[':draft'].invoke
     end
   else
-    puts "Found #{drafts.length} drafts:"
-    drafts.each_with_index { |val, index| puts "    #{index}: #{val.gsub('_drafts/', '')}" }
-    source = drafts[get_input('Which draft would you like to publish? ').to_i]
-    
-    # create a new file in _posts with the date
-    target = "_posts/#{Time.now.getlocal("-07:00").strftime('%Y-%m-%d')}-#{source.gsub('_drafts/', '')}"
-    File.open(target, 'w') do |file| file.write <<-EOS
----
-date: #{Time.now.getlocal("-07:00")}
-EOS
+    # check if "all" command was passed
+    if ARGV[1] == "all"
+      task :all do ; end
+      drafts.each do |draft|
+        publish_draft(draft)
+      end
+      puts "Published #{num} drafts."
+    else
+      puts "Found #{num} drafts:"
+      drafts.each_with_index { |val, index| puts "    #{index}: #{val.gsub('_drafts/', '')}" }
+      source = drafts[get_input('Which draft would you like to publish? ').to_i]
+      
+      target = publish_draft(source)
+      puts "Post created at #{target}"
     end
-    
-    # copy everything except opening line from draft file
-    File.readlines(source).drop(1).each do |line| 
-      File.open(target, "a+") { |file| file.write(line) }
-    end
-    
-    # delete the draft & announce the new post
-    File.delete(source)
-    puts "Post created at #{target}"
-    
   end
 end
+
+# Default task is to make a new draft
+task :default => [:draft]
